618f2d3f27a7b7e75f48fe4d92375b0d7ad3b887
Kanishk Narang <38214187+Kanishknarang@users.noreply.github.com>
2019-03-08 12:21:37 +0530

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -133,8 +133,8 @@ LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'kanishknarang123@gmail.com'
-EMAIL_HOST_PASSWORD = 'mfooswkqplklgegd'
+EMAIL_HOST_PASSWORD = ''
