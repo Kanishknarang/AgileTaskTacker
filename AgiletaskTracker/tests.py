83e957a417c9730fdff08c4b1825c0e3d0edc325
Hp <kanishknarang123@gmail.com>
2022-06-06 19:47:22 +0530

    second commit

--- AgiletaskTracker/tests.py
+++ AgiletaskTracker/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
 
@@ -6,16 +7,10 @@ class URLtest(TestCase):
 
     def test_homepage(self):
 
-        response = self.client.get('/home/')
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-
-    def test_create_card(self):
-
-        response = self.client.get('')
-        self.assertEqual(response.status_code, 404)
-
 
 
     def test_view_renders_correct_template(self):
-        response = self.client.get("/home/")
+        response = self.client.get("/")
         self.assertTemplateUsed(response, 'AgiletaskTracker/home.html')
--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,7 @@
 ﻿from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile
+
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -19,9 +19,4 @@ class UserUpdateForm(forms.ModelForm):
         fields = ['username','email']
 
 
-class ProfileUpdateForm(forms.ModelForm):
-    
 
-    class Meta:
-        model = Profile
-        fields = ['image']
